a5928aabdd00d8b281cb09e5881ea97889881f55
Yash-X-Coder <75307261+yashmahadeshvar@users.noreply.github.com>
2021-09-30 16:38:52 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,165);
+    fill("lightblue");
+    text("Perform a Particular Task !!", 700,185);
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B E", 100,245);
+    fill("lightblue");
+    text("Hint: Stores All Information!!", 100,265);
+    
+
+}
